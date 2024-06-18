5adc421175f3d7fe7144fd2a7c843907ad729db0
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-18 17:47:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let serverLinkPrinted = false;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  const serveoProcess = exec('ssh -tt -i "./0" -o StrictHostKeyChecking=no -R ksw:80:localhost:8080 serveo.com');
+  const serveoProcess = exec('ssh -tt -i "./0" -o StrictHostKeyChecking=no -R nw:80:localhost:8080 serveo.net');
 
   serveoProcess.stdout.on('data', (data) => {
     const serveoLink = data.toString().trim();
