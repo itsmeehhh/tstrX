ff32396813396a180d33cccb351e60096ee90875
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-18 14:55:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const { exec } = require('child_process');
+const port = 8080;
+const express = require("express");
+const app = express();
+app.get("/", function (_req, res) {
+  res.sendStatus(200);
+});
 
+
+
+let serverLinkPrinted = false; 
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  const serveoProcess = exec('ssh -tt -i "./0" -o StrictHostKeyChecking=no -R fb-trt:80:localhost:8080 serveo.net');
+
+  serveoProcess.stdout.on('data', (data) => {
+    const serveoLink = data.toString().trim();
+    if (!serverLinkPrinted) {
+      console.log(`Serveo link: ${serveoLink}`);
+      serverLinkPrinted = true;
+    }
+  });
+
+  serveoProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  serveoProcess.on('close', (code) => {
+    console.log(`Serveo process exited with code ${code}`);
+  });
+});
