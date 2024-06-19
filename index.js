6dcd934da3f1ef825b4495a59c04d86e14ebaf71
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-19 15:33:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,27 +6,47 @@ app.get("/", function (_req, res) {
   res.sendStatus(200);
 });
 
-
-
-let serverLinkPrinted = false; 
+let serverLinkPrinted = false;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  const serveoProcess = exec('ssh -tt -i "./0" -o StrictHostKeyChecking=no -R nw:80:localhost:8080 serveo.net');
+  
+  const tryLocaltunnel = () => {
+    const subdomain = 'oussamatest'; // اختر اسم نطاق مخصص
+    const localtunnelProcess = exec(`lt --port ${port} --subdomain ${subdomain}`);
 
-  serveoProcess.stdout.on('data', (data) => {
-    const serveoLink = data.toString().trim();
-    if (!serverLinkPrinted) {
-      console.log(`Serveo link: ${serveoLink}`);
-      serverLinkPrinted = true;
-    }
-  });
+    localtunnelProcess.stdout.on('data', (data) => {
+      const localtunnelLink = data.toString().trim();
+      if (!serverLinkPrinted) {
+        console.log(`Localtunnel link: ${localtunnelLink}`);
+        serverLinkPrinted = true;
+      }
+    });
 
-  serveoProcess.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    localtunnelProcess.stderr.on('data', (data) => {
+      const errorMessage = data.toString().trim();
+      console.error(`stderr: ${errorMessage}`);
+      const knownErrors = [
+        "Error: connection refused",
+        "localtunnel server could not be reached"
+      ];
+      if (knownErrors.some(error => errorMessage.includes(error))) {
+        console.log('Error detected, retrying...');
+        serverLinkPrinted = false;
+        localtunnelProcess.kill();
+        
+        // تنظيف الكونسول
+        process.stdout.write('\x1Bc');
+        
+        // إعادة محاولة الاتصال
+        tryLocaltunnel();
+      }
+    });
 
-  serveoProcess.on('close', (code) => {
-    console.log(`Serveo process exited with code ${code}`);
-  });
+    localtunnelProcess.on('close', (code) => {
+      console.log(`Localtunnel process exited with code ${code}`);
+    });
+  };
+
+  tryLocaltunnel();
 });
